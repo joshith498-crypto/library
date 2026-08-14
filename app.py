cf25ccb77adef0b8507b65bb087e722ad9c8b93c
joshith498-crypto <joshith498@gmail.com>
2026-08-14 20:08:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# The ONLY job of your Python backend now is to serve the 
-# beautiful macOS interface. Google Firebase handles the rest.
+
 @app.route('/')
 def index():
     return render_template('index.html')
